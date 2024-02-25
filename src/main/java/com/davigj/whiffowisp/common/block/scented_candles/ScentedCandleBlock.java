99ef1ef3c87bcc018f1cd1da034329aeec40e63e
DavigJ <david.j.jung@gmail.com>
2024-02-24 20:45:30 -0500

    figuring out scented candle blockstate gen

--- src/main/java/com/davigj/whiffowisp/common/block/scented_candles/ScentedCandleBlock.java
+++ src/main/java/com/davigj/whiffowisp/common/block/scented_candles/ScentedCandleBlock.java
@@ -1,16 +1,30 @@
 package com.davigj.whiffowisp.common.block.scented_candles;
 
+import com.teamabnormals.blueprint.core.util.BlockUtil;
+import net.mehvahdjukaar.supplementaries.common.block.blocks.BubbleBlock;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CandleBlock;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.ObserverBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
 
-import static com.davigj.whiffowisp.core.other.WOWBlockStatements.TRIMMED;
+import static com.davigj.whiffowisp.core.other.WOWConstants.BOTTLED;
+import static com.davigj.whiffowisp.core.other.WOWConstants.TRIMMED;
 
 public class ScentedCandleBlock extends CandleBlock {
 
@@ -20,7 +34,8 @@ public class ScentedCandleBlock extends CandleBlock {
                 .setValue(TRIMMED, Boolean.valueOf(false))
                 .setValue(CANDLES, Integer.valueOf(1))
                 .setValue(LIT, Boolean.valueOf(false))
-                .setValue(WATERLOGGED, Boolean.valueOf(false)));
+                .setValue(WATERLOGGED, Boolean.valueOf(false))
+                .setValue(BOTTLED, Boolean.valueOf(false)));
     }
 
     public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
@@ -33,7 +48,7 @@ public class ScentedCandleBlock extends CandleBlock {
     public void affect(Level level, BlockPos pos, BlockState state, Entity entity) {    }
 
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(new Property[]{TRIMMED, LIT, CANDLES, WATERLOGGED});
+        builder.add(new Property[]{TRIMMED, LIT, CANDLES, WATERLOGGED, BOTTLED});
     }
 
 }
--- src/main/java/com/davigj/whiffowisp/core/data/client/WOWBlockStateProvider.java
+++ src/main/java/com/davigj/whiffowisp/core/data/client/WOWBlockStateProvider.java
@@ -1,9 +1,12 @@
 package com.davigj.whiffowisp.core.data.client;
 
+import com.davigj.whiffowisp.common.block.scented_candles.ScentedCandleBlock;
 import com.davigj.whiffowisp.core.WhiffOWisp;
+import com.davigj.whiffowisp.core.other.WOWConstants;
 import com.davigj.whiffowisp.core.registry.WOWBlocks;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.AbstractCandleBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CandleBlock;
 import net.minecraft.world.level.block.FenceGateBlock;
@@ -33,28 +36,56 @@ public class WOWBlockStateProvider extends BlockStateProvider {
     @Override
     protected void registerStatesAndModels() {
 //        this.trimmedCandleBlock((CandleBlock) WOWBlocks.TRIMMED_CANDLE.get(), new ResourceLocation("minecraft:block/candle"), new ResourceLocation("minecraft:block/candle_lit"));
+        this.scentedCandle((ScentedCandleBlock) WOWBlocks.DISTANT_SONG_SCENTED_CANDLE.get(), "whiffowisp:block/distant_song_scented_candle");
+    }
+    private void scentedCandle(ScentedCandleBlock block, String candleName) {
+        ModelFile smallCandle = models().withExistingParent(name(block) + "_small", mcLoc("whiffowisp:block/small_scented_candle"))
+                .texture("all", new ResourceLocation(candleName + "_small")).texture("particle", new ResourceLocation(candleName + "_small"));
+        ModelFile mediumCandle = models().withExistingParent(name(block) + "_medium", mcLoc("whiffowisp:block/medium_scented_candle"))
+                .texture("all", new ResourceLocation(candleName + "_medium")).texture("particle", new ResourceLocation(candleName + "_medium"));
+        ModelFile bigCandle = models().withExistingParent(name(block) + "_big", mcLoc("whiffowisp:block/big_scented_candle"))
+                .texture("all", new ResourceLocation(candleName + "_big")).texture("particle", new ResourceLocation(candleName + "_big"));
+        ModelFile largeCandle = models().withExistingParent(name(block) + "_large", mcLoc("whiffowisp:block/large_scented_candle"))
+                .texture("all", new ResourceLocation(candleName + "_large")).texture("particle", new ResourceLocation(candleName + "_large"));
+        scentedCandle(block, smallCandle, mediumCandle, bigCandle, largeCandle);
     }
 
-    private void trimmedCandleBlock(CandleBlock block, ResourceLocation candle, ResourceLocation litCandle) {
+    public void scentedCandle(CandleBlock block, ModelFile one, ModelFile two, ModelFile three, ModelFile four) {
+        getVariantBuilder(block).forAllStatesExcept(state -> {
+            ModelFile model = one;
+            switch (state.getValue(CandleBlock.CANDLES)) {
+                case 2 -> model = two;
+                case 3 -> model = three;
+                case 4 -> model = four;
+            }
+            return ConfiguredModel.builder()
+                    .modelFile(model)
+                    .build();
+        }, CandleBlock.WATERLOGGED, WOWConstants.BOTTLED, AbstractCandleBlock.LIT, WOWConstants.TRIMMED);
+    }
+
+    private void trimmedCandle(ScentedCandleBlock block, ResourceLocation candleTexture, ResourceLocation litCandleTexture) {
         ModelFile trimmedCandle = models().withExistingParent(name(block), mcLoc("minecraft:block/template_candle"))
-                .texture("all", candle).texture("particle", candle);
+                .texture("all", candleTexture).texture("particle", candleTexture);
         ModelFile trimmedTwoCandle = models().withExistingParent(name(block) + "_two_candles", mcLoc("minecraft:block/template_two_candles"))
-                .texture("all", candle).texture("particle", candle);
+                .texture("all", candleTexture).texture("particle", candleTexture);
         ModelFile trimmedThreeCandle = models().withExistingParent(name(block) + "_three_candles", mcLoc("minecraft:block/template_three_candles"))
-                .texture("all", candle).texture("particle", candle);
+                .texture("all", candleTexture).texture("particle", candleTexture);
         ModelFile trimmedFourCandle = models().withExistingParent(name(block) + "_four_candles", mcLoc("minecraft:block/template_four_candles"))
-                .texture("all", candle).texture("particle", candle);
+                .texture("all", candleTexture).texture("particle", candleTexture);
         ModelFile trimmedCandleLit = models().withExistingParent(name(block) + "_lit", mcLoc("minecraft:block/template_candle"))
-                .texture("all", litCandle).texture("particle", litCandle);
+                .texture("all", litCandleTexture).texture("particle", litCandleTexture);
         ModelFile trimmedTwoCandleLit = models().withExistingParent(name(block) + "_two_candles_lit", mcLoc("minecraft:block/template_two_candles"))
-                .texture("all", litCandle).texture("particle", litCandle);
+                .texture("all", litCandleTexture).texture("particle", litCandleTexture);
         ModelFile trimmedThreeCandleLit = models().withExistingParent(name(block) + "_three_candles_lit", mcLoc("minecraft:block/template_three_candles"))
-                .texture("all", litCandle).texture("particle", litCandle);
+                .texture("all", litCandleTexture).texture("particle", litCandleTexture);
         ModelFile trimmedFourCandleLit = models().withExistingParent(name(block) + "_four_candles_lit", mcLoc("minecraft:block/template_four_candles"))
-                .texture("all", litCandle).texture("particle", litCandle);
+                .texture("all", litCandleTexture).texture("particle", litCandleTexture);
         trimmedCandleBlock(block, trimmedCandle, trimmedCandleLit, trimmedTwoCandle, trimmedTwoCandleLit,
                 trimmedThreeCandle, trimmedThreeCandleLit, trimmedFourCandle, trimmedFourCandleLit);
     }
+
+
 
     public void trimmedCandleBlock(CandleBlock block, ModelFile one, ModelFile oneLit, ModelFile two, ModelFile twoLit,
                                    ModelFile three, ModelFile threeLit, ModelFile four, ModelFile fourLit) {
@@ -63,20 +94,13 @@ public class WOWBlockStateProvider extends BlockStateProvider {
             boolean lit = state.getValue(CandleBlock.LIT);
             model = lit ? oneLit : model;
             switch (state.getValue(CandleBlock.CANDLES)) {
-                case 2 -> {
-                    model = lit ? twoLit : two;
-                }
-                case 3 -> {
-                    model = lit ? threeLit : three;
-                }
-                case 4 -> {
-                    model = lit ? fourLit : four;
-                }
+                case 2 -> model = lit ? twoLit : two;
+                case 3 -> model = lit ? threeLit : three;
+                case 4 -> model = lit ? fourLit : four;
             }
             return ConfiguredModel.builder()
                     .modelFile(model)
                     .build();
         }, CandleBlock.WATERLOGGED);
     }
-
 }
